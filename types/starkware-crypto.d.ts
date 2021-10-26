03abd97eb0e10c74eb3c30416d1ea3c5c3e3276b
himanshuchawla009 <himanshuchawla2014@gmail.com>
2021-10-26 17:42:19 +0530

    types added for sign, verify and pedersen function

--- types/starkware-crypto.d.ts
+++ types/starkware-crypto.d.ts
@@ -1,5 +1,8 @@
-import type { ec as EllipticCurve } from 'elliptic'
+import type { ec, ec as EllipticCurve } from 'elliptic'
 
 export declare function getAccountPath(layer: string, application:string, ethereumAddress: string, index: string | number): string
 export declare function getKeyPairFromPath(mnemonic: string, path: string): EllipticCurve.KeyPair
+export declare function sign(privateKey: ec.KeyPair, msgHash: string): ec.Signature
+export declare function verify(publicKey:  ec.KeyPair, msgHash: string, msgSignature: ec.Signature): ec.Signature
+export declare function pedersen(input: any): string
 export declare const ec: EllipticCurve;

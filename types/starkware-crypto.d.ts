c3f8176342941c67e011cd75129bd8981bb25600
Lionell Briones <llenoil@gmail.com>
2022-03-22 09:08:51 +0800

    Added grindkey

--- types/starkware-crypto.d.ts
+++ types/starkware-crypto.d.ts
@@ -2,6 +2,7 @@ import type { ec as EllipticCurve } from 'elliptic'
 
 export declare function getAccountPath(layer: string, application:string, ethereumAddress: string, index: string | number): string
 export declare function getKeyPairFromPath(mnemonic: string, path: string): EllipticCurve.KeyPair
+export declare function grindKey(keySeed: string, keyValLimit: BN): string
 export declare function sign(privateKey: EllipticCurve.KeyPair, msgHash: string): EllipticCurve.Signature
 export declare function verify(publicKey:  EllipticCurve.KeyPair, msgHash: string, msgSignature: EllipticCurve.Signature): EllipticCurve.Signature
 export declare function pedersen(input: any): string

ec4b34b48b4f67e37cda0535da458a533e6ab19d
Lionell Briones <llenoil@gmail.com>
2022-03-23 22:55:03 +0800

    initial ts update

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,48 @@
-const path = require('path')
-const ESLintPlugin = require('eslint-webpack-plugin')
+const path = require("path");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
-const pkg = require('./package.json')
+const pkg = require("./package.json");
 
-const pkgName = 'starkware-crypto'
-const libraryName = pkgName.charAt(0).toUpperCase() + pkgName.slice(1)
+const pkgName = "starkware-crypto";
+const libraryName = pkgName.charAt(0).toUpperCase() + pkgName.slice(1);
 
-const { NODE_ENV = 'production' } = process.env
+const { NODE_ENV = "production" } = process.env;
 
 const baseConfig = {
   mode: NODE_ENV,
-  devtool: 'source-map',
-  entry: './src/index.js',
-  target: 'web',
+  devtool: "source-map",
+  entry: "./src/index.ts",
+  target: "web",
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
     library: libraryName,
   },
   resolve: {
     alias: {
-    //   'bn.js': path.resolve(__dirname, 'node_modules/bn.js'),
+      //   'bn.js': path.resolve(__dirname, 'node_modules/bn.js'),
     },
   },
   module: {
     rules: [],
   },
   node: {
-    vm: 'empty',
+    vm: "empty",
   },
-}
+};
 
 const optimization = {
   optimization: {
     minimize: false,
   },
-}
+};
 
 const babelLoader = {
   test: /\.m?js$/,
   exclude: /(node_modules|bower_components)/,
   use: {
-    loader: 'babel-loader',
+    loader: "babel-loader",
   },
-}
+};
 
 const cjsConfig = {
   ...baseConfig,
@@ -50,20 +50,20 @@ const cjsConfig = {
   output: {
     ...baseConfig.output,
     filename: `${pkgName}.cjs.js`,
-    libraryTarget: 'commonjs2',
+    libraryTarget: "commonjs2",
   },
   module: {
     rules: [babelLoader],
   },
   plugins: [
     new ESLintPlugin({
-      files: 'src',
+      files: "src",
     }),
   ],
   externals: [...Object.keys(pkg.dependencies), /^(@babel\/runtime)/i],
   node: {
     ...baseConfig.node,
   },
-}
+};
 
-module.exports = [cjsConfig]
+module.exports = [cjsConfig];
